9fdc00e0086779b2ee8d802be92a5992cd07c016
Paul Nikitochkin <pftg@users.noreply.github.com>
2018-03-31 19:39:07 +0300

    Fixed deprecated wrning

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module VuejsRorSetup
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
