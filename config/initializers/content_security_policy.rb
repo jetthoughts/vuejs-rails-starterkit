8ffcc70c2bc342a84305a5ebcb62475ebbef3aa5
Paul Keen <pftg@users.noreply.github.com>
2018-06-03 18:44:36 +0300

    Enable Wepbacker/Vue

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -23,3 +23,12 @@
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
 # Rails.application.config.content_security_policy_report_only = true
+
+Rails.application.config.content_security_policy do |policy|
+  if Rails.env.development?
+    policy.script_src :self, :https, :unsafe_eval
+  else
+    policy.script_src :self, :https
+  end
+end
+
