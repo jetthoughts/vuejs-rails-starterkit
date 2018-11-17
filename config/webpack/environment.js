e601bcb6e7c675b27ab2958b753ec3c550f106b6
Paul Keen <pftg@noreply.github.com>
2018-11-17 10:55:53 +0200

    Upgraded webpacker to 4

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
 const vue =  require('./loaders/vue')
 
+environment.plugins.append('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.append('vue', vue)
 module.exports = environment
