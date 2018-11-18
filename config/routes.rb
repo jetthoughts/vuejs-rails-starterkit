40cfe6c5aad4634cc80650b854073acda19616ee
Paul Keen <pftg@noreply.github.com>
2018-11-18 22:15:34 +0200

    Added page with vue.js hello commoponent

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+root 'landing#index'
+  get 'landing/index'
+root 'landing#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
