7f6a21ed00ca902437a9d32cea74ef6c0b6988be
Paul Keen <62-pkeen@users.noreply.rtfm.corsis.com>
2020-04-07 12:27:18 +0300

    Add serviceworker example for offline case

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 
+import 'serviceworker-companion'
+
 import 'initializers/polyfills'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
