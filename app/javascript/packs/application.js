a71c851f4896d38b7d595cac2972b8648887a375
Paul Keen <62-pkeen@users.noreply.rtfm.corsis.com>
2020-04-07 12:28:19 +0300

    Setup Turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 import 'serviceworker-companion'
 
 import 'initializers/polyfills'
+import 'initializers/turbolinks'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
