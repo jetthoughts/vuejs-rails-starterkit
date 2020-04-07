d44361dd6448cabe7dbe2549f2d3a3ec5b529307
Paul Keen <62-pkeen@users.noreply.rtfm.corsis.com>
2020-04-07 12:24:56 +0300

    Add polyfills to entry points

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 
+import 'initializers/polyfills'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
