bd8697ad9ae96f54f9f22ed192b365938aabd59f
Paul Keen <125715+pftg@users.noreply.github.com>
2021-01-04 21:18:14 +0200

    Generates Ruby on Rails Application with Vue.js

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,16 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import * as ActiveStorage from "@rails/activestorage"
+
+Rails.start()
+ActiveStorage.start()
+
+
+import 'serviceworker-companion'
+import 'initializers/polyfills'
+import 'initializers/turbolinks'
+import 'initializers/adminlte'
